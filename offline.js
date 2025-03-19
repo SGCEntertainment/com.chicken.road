1cad9e3a8c332ad1ceb1dbf8c12c7285be8a336e
ZloyShcolnik <zloalways7@gmail.com>
2025-03-19 18:13:10 +0300

    init

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1742397118,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/road-sheet0.png",
+		"images/decor-sheet0.png",
+		"images/roadground-sheet0.png",
+		"images/roaddecor-sheet0.png",
+		"images/coingray-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
